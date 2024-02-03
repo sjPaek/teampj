4d6a4eeb016fe2a4e04397505c1b772a10a833b7
SJ <90229606+sjPaek@users.noreply.github.com>
2024-02-03 14:42:09 +0900

    project reset

---
+++ src/main/java/com/teampj/project/webpage/ServletInitializer.java
@@ -0,0 +1,13 @@
+package com.teampj.project.webpage;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(WebpageApplication.class);
+	}
+
+}
