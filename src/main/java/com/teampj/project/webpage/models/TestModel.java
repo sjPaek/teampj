b551445a356ff7552ee45f56936ac44564062e3e
SJ <90229606+sjPaek@users.noreply.github.com>
2024-02-03 16:40:16 +0900

    lib 수정 bootstrap 테스트

---
+++ src/main/java/com/teampj/project/webpage/models/TestModel.java
@@ -0,0 +1,12 @@
+package com.teampj.project.webpage.models;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+public class TestModel {
+    private int seq;
+}
