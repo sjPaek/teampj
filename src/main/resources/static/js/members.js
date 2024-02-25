02e869648665abe1e0c6391dac856e748f649343
SJ <90229606+sjPaek@users.noreply.github.com>
2024-02-25 23:03:31 +0900

    member js 데이터 불러오기

--- src/main/resources/static/js/members.js
+++ src/main/resources/static/js/members.js
@@ -1,3 +1,13 @@
+const funcGetMembersList = () => {
+    const url = "/members/list";
+    const getting = $.get(url);
+    getting.done((data) => {
+        console.log(data);
+    })
+    getting.fail(() => {
+        showMessage('Error', '통신 에러 관리자에게 문의해주세요');
+    })
+}
 $(() => {
-    console.log("member");
+    funcGetMembersList();
 })
